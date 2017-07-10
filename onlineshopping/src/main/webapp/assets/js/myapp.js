ebcbbeb3b4caddae4799d42099f945b4bfa449b8
neerajpal0 <getneeraj11@gmail.com>
2017-07-10 18:40:40 +0530

    footer setup completed

---
+++ onlineshopping/src/main/webapp/assets/js/myapp.js
@@ -0,0 +1,19 @@
+$(function() {
+	//solving the active menu problem
+	//menu used in page.jsp
+	//case 'About Us' used from pageController.java
+	switch (menu) {
+
+	case 'About Us':
+		$('#about').addClass('active');
+		break;
+
+	case 'Contact Us':
+		$('#contact').addClass('active');
+
+	case 'Home':
+		$('#home').addClass('active');
+
+	}
+
+});
