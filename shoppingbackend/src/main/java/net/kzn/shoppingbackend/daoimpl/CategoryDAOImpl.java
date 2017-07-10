6abe55308ebb5cf3278f13f646d3ce6711287baa
neerajpal0 <getneeraj11@gmail.com>
2017-07-10 23:57:26 +0530

    commited with connected shoppingbackend

--- shoppingbackend/src/main/java/net/kzn/shoppingbackend/daoimpl/CategoryDAOImpl.java
+++ shoppingbackend/src/main/java/net/kzn/shoppingbackend/daoimpl/CategoryDAOImpl.java
@@ -3,40 +3,41 @@ package net.kzn.shoppingbackend.daoimpl;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
 import net.kzn.shoppingbackend.dao.CategoryDAO;
 import net.kzn.shoppingbackend.dto.Category;
 
+@Repository("categoryDAO")
 public class CategoryDAOImpl implements CategoryDAO {
-	
-	
-	@Override
-	public List<Category> list() {
-		
-		
-		List<Category> categories=new ArrayList<Category>();
-		
-		Category category=new Category();
+
+	private static List<Category> categories = new ArrayList<Category>();
+
+	static {
+		Category category = new Category();
 		category.setId(1);
 		category.setName("Television");
 		category.setDescription("Description for televion");
 		category.setImageURL("CAT_1.png");
 		categories.add(category);
-		
-		
-	    category=new Category();
+
+		category = new Category();
 		category.setId(2);
 		category.setName("Mobile");
 		category.setDescription("Description for Mobile");
 		category.setImageURL("CAT_2.png");
 		categories.add(category);
-		
-		category=new Category();
+
+		category = new Category();
 		category.setId(2);
 		category.setName("Laptop");
 		category.setDescription("Description for Laptop");
 		category.setImageURL("CAT_3.png");
 		categories.add(category);
-		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public List<Category> list() {
 		return categories;
 	}
 
